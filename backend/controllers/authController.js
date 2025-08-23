1c74801158a2d35653edb524509b2e2b9ba30d2a
vikashsathe <vikashsathe83@gmail.com>
2025-08-23 23:09:05 +0530

    some changes

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,31 +1,94 @@
-const User = require("../models/User")
-const bcrypt = require("bcrypt")
+// const User = require("../models/User")
+// const bcrypt = require("bcrypt")
+// const jwt = require("jsonwebtoken");
+
+// // Register
+// exports.register = async (req, res) => {
+//     try {
+//         const { name, email, password } = req.body
+
+//         // check if user exists
+//         const existingUser = await User.findOne({ email })
+//         if (existingUser) {
+//             return res.status(400).send("Email already registered")
+//         }
+
+//         // hash password
+//         const hashedPassword = await bcrypt.hash(password, 10)
+
+//         // save new user
+//         const newUser = new User({ name, email, password: hashedPassword })
+//         await newUser.save()
+
+//         res.status(201).send("User registered successfully")
+//     } catch (err) {
+//         res.status(500).send("Error: " + err.message)
+//     }
+// }
+
+
+// // Login
+// exports.login = async (req, res) => {
+//   try {
+//     const { email, password } = req.body;
+
+//     const user = await User.findOne({ email });
+//     if (!user) return res.status(400).send("User not found");
+
+//     const isMatch = await bcrypt.compare(password, user.password);
+//     if (!isMatch) return res.status(400).send("Invalid password");
+
+//     // create JWT token
+//     const token = jwt.sign(
+//       { _id: user._id, name: user.name, email: user.email },
+//       "your_jwt_secret",
+//       { expiresIn: "7d" }
+//     );
+
+//     // Send back token + user (without password)
+//     res.json({
+//       message: "Login successful",
+//       token,
+//       user: {
+//         _id: user._id,
+//         name: user.name,
+//         email: user.email,
+//       },
+//     });
+//   } catch (err) {
+//     res.status(500).send("Error: " + err.message);
+//   }
+// };
+
+
+
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 // Register
 exports.register = async (req, res) => {
-    try {
-        const { name, email, password } = req.body
+  try {
+    const { name, email, password } = req.body;
 
-        // check if user exists
-        const existingUser = await User.findOne({ email })
-        if (existingUser) {
-            return res.status(400).send("Email already registered")
-        }
-
-        // hash password
-        const hashedPassword = await bcrypt.hash(password, 10)
-
-        // save new user
-        const newUser = new User({ name, email, password: hashedPassword })
-        await newUser.save()
-
-        res.status(201).send("User registered successfully")
-    } catch (err) {
-        res.status(500).send("Error: " + err.message)
+    // check if user exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "Email already registered" });
     }
-}
 
+    // hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // save new user
+    const newUser = new User({ name, email, password: hashedPassword });
+    await newUser.save();
+
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (err) {
+    res.status(500).json({ message: "Error: " + err.message });
+  }
+};
 
 // Login
 exports.login = async (req, res) => {
@@ -33,15 +96,15 @@ exports.login = async (req, res) => {
     const { email, password } = req.body;
 
     const user = await User.findOne({ email });
-    if (!user) return res.status(400).send("User not found");
+    if (!user) return res.status(400).json({ message: "User not found" });
 
     const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return res.status(400).send("Invalid password");
+    if (!isMatch) return res.status(400).json({ message: "Invalid password" });
 
     // create JWT token
     const token = jwt.sign(
       { _id: user._id, name: user.name, email: user.email },
-      "your_jwt_secret",
+      process.env.JWT_SECRET || "your_jwt_secret",
       { expiresIn: "7d" }
     );
 
@@ -56,6 +119,7 @@ exports.login = async (req, res) => {
       },
     });
   } catch (err) {
-    res.status(500).send("Error: " + err.message);
+    res.status(500).json({ message: "Error: " + err.message });
   }
 };
+
